e9ab3bc1499fca6fa36967fdc924f4330bdba0c7
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-27 22:46:42 +0200

    Opgave a og b (ikke færdige)

---
+++ Cs006_Datoopgave/Dato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs006_Datoopgave
+{
+    class Dato
+    {
+        private int datoen;
+
+        public Dato(int aar, int maaned, int dag)
+        {
+            datoen = aar + maaned + dag;
+        }
+
+        public int GetAar()
+        {
+            int aar = this.datoen;
+            return aar;
+        }
+    }
+}
